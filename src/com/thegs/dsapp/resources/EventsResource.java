47dbe84059672b520ad7395927ee6d284be6343b
hariv1505 <hariharen.veerakumar@gmail.com>
2014-10-27 15:19:50 +1100

    Added all convertors - TODO xsd files

--- src/com/thegs/dsapp/resources/EventsResource.java
+++ src/com/thegs/dsapp/resources/EventsResource.java
@@ -1,5 +1,6 @@
 package com.thegs.dsapp.resources;
 
+import java.util.Collection;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
@@ -31,35 +32,19 @@ public class EventsResource {
 	Request request;
 	String AUTH_KEY = "abc123";
 	
-	// Return the list of events to the user in the browser
-	@GET
-	@Produces(MediaType.TEXT_XML)
-	public Map<String, Event> getEventsBrowser(
-			@HeaderParam("Auth") String auth,
-			@Context final HttpServletResponse response) {
-		if(auth == null || !auth.equals(AUTH_KEY)){
-			response.setHeader("authorised", "false");
-			throw new WebApplicationException(Response.status(Response.Status.FORBIDDEN.getStatusCode())
-					.entity("Forbidden")
-					.header("authorised", "false").build());
-		}
-		
-		return EventDao.instance.getStore(); 
-	}
-	
 	// Return the list of events for client applications/programs
 	@GET
-	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-	public Map<String, Event> getEvents(@HeaderParam("Auth") String auth,
+	public Collection<Event> getEvents(@HeaderParam("Auth") String auth,
 		@Context final HttpServletResponse response) {
 		if(auth == null || !auth.equals(AUTH_KEY)){
 			response.setHeader("authorised", "false");
-			throw new WebApplicationException(Response.status(Response.Status.FORBIDDEN.getStatusCode())
+			throw new WebApplicationException(Response
+					.status(Response.Status.FORBIDDEN.getStatusCode())
 					.entity("Forbidden")
 					.header("authorised", "false").build());
 		}
 
-		return EventDao.instance.getStore();	
+		return EventDao.instance.getStore().values();	
 	}
 	
 	@GET
@@ -69,7 +54,8 @@ public class EventsResource {
 			@Context final HttpServletResponse response) {
 		if(auth == null || !auth.equals(AUTH_KEY)){
 			response.setHeader("authorised", "false");
-			throw new WebApplicationException(Response.status(Response.Status.FORBIDDEN.getStatusCode())
+			throw new WebApplicationException(Response
+					.status(Response.Status.FORBIDDEN.getStatusCode())
 					.entity("Forbidden")
 					.header("authorised", "false").build());
 		}
@@ -79,43 +65,6 @@ public class EventsResource {
 		return String.valueOf(count);
 	}
 	
-    // Client should set Content Type accordingly
-	/*@PUT
-	@Produces(MediaType.TEXT_HTML)
-	@Path("{event}")
-	public String newEvent(
-			@PathParam("event") String id,
-			@Context HttpServletResponse servletResponse,
-			@HeaderParam("Auth") String auth
-	) throws IOException {
-		if(auth == null || !auth.equals(AUTH_KEY)){
-			servletResponse.setHeader("authorised", "false");
-			throw new WebApplicationException(Response.status(Response.Status.FORBIDDEN.getStatusCode())
-					.entity("Forbidden")
-				.header("authorised", "false").build());
-		} else {
-			String uri = null; //TODO: what is this?
-			Event e = new Event(id, uri);
-			EventDao.instance.addEvent(e);
-		}
-		
-		// Redirect to some HTML page  
-		// You need to create this file under WEB-INF
-		//servletResponse.setHeader("cost", cost);
-		//servletResponse.setHeader("uri", "/events/" + id);
-		//servletResponse.setHeader("authorised", "true");
-		//servletResponse.sendRedirect("../create_event.html");
-		
-		return "done";
-	}*/
-	
-	
-	// Important to note that this Path annotation define.
-	// This will match xxx.xxx.xxx/rest/events/{event}
-	// It says 'the thing that comes after events/ is a parameter
-	// and it is passed to the EventResource class for processing
-	// e.g., http://localhost:8080/cs9322.simple.rest.events/rest/events/3
-        // This matches this method which returns EventResource.
 	@Path("{event}")
 	public EventResource getEvent(
 			@PathParam("event") String id,
@@ -123,7 +72,8 @@ public class EventsResource {
 			@Context final HttpServletResponse response) {
 		if(auth == null || !auth.equals(AUTH_KEY)){
 			response.setHeader("authorised", "false");
-			throw new WebApplicationException(Response.status(Response.Status.FORBIDDEN.getStatusCode())
+			throw new WebApplicationException(Response
+					.status(Response.Status.FORBIDDEN.getStatusCode())
 					.entity("Forbidden")
 					.header("authorised", "false").build());
 		}
